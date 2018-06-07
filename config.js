d22555e1abedf0d0e099a05697d5fdae1a5dc79f
Hyungsuk Kang <hskang9@gmail.com>
2018-06-07 23:38:37 +0900

    Fix for each of undefined error
    Fix GH-1

--- config.js
+++ config.js
@@ -2,13 +2,16 @@
 const DIFFICULTY = 4;
 
 // Initial amount of coin in genesis block
-const INITIAL_BALANCE = 500;
+const INITIAL_BALANCE = 500000000000000;
 
 // Mining reward
 const MINING_REWARD = 50;
 
+// Listening port
+const PORT = 5000;
+
 // mining nodes
-const NODE_ADDRESSES = [];
+const NODE_ADDRESSES = [`localhost:${PORT}`];
 
 
 
